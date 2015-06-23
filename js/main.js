cc8d3764191b247363a5ac6ee5adc35a75cfa4cb
955NONAME <louis@olpp.info>
2015-06-23 20:45:21 +0200

    upldate

--- js/main.js
+++
@@ -1,9 +0,0 @@
-require(['scaleBody', 'flashcards', 'domReady!'], function (scaleBody, FlashCards) {
-    var App = new FlashCards();
-    App.init();
-
-    //hack to get active state to work on webkit
-    this.touchstart = function (e) {};
-
-    scaleBody(document.getElementById('container'), 720);
-});
